327b312eb5c5b68c215a61917b4790e21aeee88d
AbseliamovEnver <a.enver.com@gmail.com>
2019-12-23 23:39:36 +0200

    Heroku commit

--- src/main/java/net/enver/itcompanydemo/security/twilio/SmsRequest.java
+++ src/main/java/net/enver/itcompanydemo/security/twilio/SmsRequest.java
@@ -3,8 +3,10 @@ package net.enver.itcompanydemo.security.twilio;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Random;
 
 @Data
 @ToString
@@ -16,9 +18,16 @@ public class SmsRequest {
     @NotBlank
     private final String message;
 
+//    @Value("${twilio.message}")
+//    private final String message;
+
     public SmsRequest(@JsonProperty("phoneNumber") String phoneNumber,
                       @JsonProperty("message") String message) {
         this.phoneNumber = phoneNumber;
         this.message = message;
+    }
+
+    public String createMessage(){
+        return message + " " + new Random().ints(90_000) + 10_000;
     }
 }
